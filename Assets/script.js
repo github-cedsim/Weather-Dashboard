715f9dbcb5998eba8d5d6dd0ef33bdfbe16ab73b
Cedric Simpkins <126218396+github-cedsim@users.noreply.github.com>
2024-02-29 22:39:28 -0600

    Update API key and added event listener for form submit and API call

--- Assets/script.js
+++ Assets/script.js
@@ -5,7 +5,44 @@ const cityInput = document.getElementById('city-input');
 const currentWeatherContainer = document.getElementById('current-weather-container');
 const forecastContainer = document.getElementById('forecast-container');
 const searchHistoryContainer = document.getElementById('search-history-container');
-const apiKey = "dbc8865c28c2ad96f8f621df6535c4d4";
+const apiKey = "2d921f47c4cc133aeee4dd3691099ae1";
 let city = ""
 let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
+// Event listener for form submit
+searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    document.querySelector(".right-side").style.display = "block";
+    city = cityInput.value;
+    // Call API to fetch weather data for the city
+    fetchWeatherData(city);
+    updateSearchHistory(city);
+});
+
+// Function to fetch weather data from API
+function fetchWeatherData(city) {
+
+    // Call API and retrieve current and forecast weather data for the city
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        // Update DOM with retrieved data using DOM manipulation
+        console.log(data)
+        updateCurrentWeather(data);
+        updateFiveForecast(data.coord);
+    })
+
+}
+
+// Function to update current weather information in DOM
+
+function updateCurrentWeather(currentWeather) {
+    // Update DOM with current weather data
+    currentWeatherContainer.innerHTML = `
+  <h2>${currentWeather.name}</h2>
+  <p class="date-image">Date: ${new Date(currentWeather.dt * 1000).toLocaleDateString()} <img src="http://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png" alt="weather icon"></p>
+  <p>Temperature: ${currentWeather.main.temp}</p>
+  <p>Humidity: ${currentWeather.main.humidity}</p>
+  <p>Wind Speed: ${currentWeather.wind.speed}</p>
+  `
+}
